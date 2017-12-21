d9cd9a6e343005188a4a266287f0911754e7e24a
Benjamin Cable <cable.benjamin@gmail.com>
2017-12-21 09:58:54 +0000

    ensure timestamps increment properly

--- migrate.go
+++ migrate.go
@@ -20,15 +20,15 @@ func migrate(cmd *cobra.Command, args []string) {
 }
 
 var autoincrementRegExp = regexp.MustCompile(`(?ms) AUTO_INCREMENT=[0-9]*\b`)
+
 func makeMigrations(tables []string, dst string) {
 	os.Mkdir(dst, 0777)
+	now := time.Now().Unix()
 	for _, table := range tables {
 		// get the create statement
 		row := database.QueryRow(fmt.Sprintf("SHOW CREATE TABLE `%s`", table))
 		var tbl, stmt string
 		row.Scan(&tbl, &stmt)
-
-		now := time.Now().Unix()
 
 		// Create the up migration
 		where := filepath.Join(dst, fmt.Sprintf("%d_create_%s.up.sql", now, tbl))
@@ -38,7 +38,7 @@ func makeMigrations(tables []string, dst string) {
 		}
 		auto := autoincrementRegExp.FindString(stmt)
 		stmt = strings.Replace(stmt, auto, "", 1)
-		_, err = up.WriteString(stmt+";")
+		_, err = up.WriteString(stmt + ";")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -52,5 +52,8 @@ func makeMigrations(tables []string, dst string) {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		// Ensure the time increments properly
+		now += 1
 	}
 }
